acf9e5090cda40242957c570e6bc021090cabd40
amart302 <154788842+amart302@users.noreply.github.com>
2023-12-31 14:16:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("open_modal").addEventListener("click" ,function(){
+    document.getElementById("my-modal").classList.add("open")
+});
+
+document.getElementById("close-my-modal").addEventListener("click" ,function(){
+    document.getElementById("my-modal").classList.remove("open")
+});
+
+document.getElementById("zakaz-modal-openbt").addEventListener("click" ,function(){
+    document.getElementById("zakaz-modal").classList.add("open")
+});
+
+document.getElementById("close-zakaz-modal").addEventListener("click" ,function(){
+    document.getElementById("zakaz-modal").classList.remove("open")
+});
+
+
